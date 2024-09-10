74cadc8f3b67472788038d2b55a37f8dec8a85ee
Dang <phanlinh2652002@gmail.com>
2024-09-10 21:17:06 +0700

    import ui login register

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import './assets/main.css'
-
+import './assets/css/bootstrap.min.css'
+import './assets/css/aos.css'
+import './assets/css/style.css'
+import './assets/css/daterangepicker.css'
+import './assets/css/styleLogin.css'
+import './assets/css/jquery.fancybox.min.css'
+import './assets/css/owl.carousel.min.css'
+import './assets/fonts/icomoon/style.css'
+import './assets/fonts/flaticon/font/flaticon.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
